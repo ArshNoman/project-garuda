c88382c7bb89cbb4d49a1baf177870bac623ca3c
arsh <arshnoman2270@gmail.com>
2025-07-16 00:45:22 -0400

    main.py test for SLAM module placeholder

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
-# src/main.py
-from perception.tracking import ObjectTracker
-import cv2
+# from perception.tracking import ObjectTracker
+# import cv2
+#
+# tracker = ObjectTracker()
+# tracker.start()
+#
+# while True:
+#     frame, detections = tracker.get_detections()
+#     cv2.imshow("YOLOv6 Detection", frame)
+#
+#     for det in detections:
+#         print(det)
+#
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+#
+# tracker.stop()
+# cv2.destroyAllWindows()
 
-tracker = ObjectTracker()
-tracker.start()
+from navigation.slam_module import SLAMModule
+import time
 
-while True:
-    frame, detections = tracker.get_detections()
-    cv2.imshow("YOLOv6 Detection", frame)
+slam = SLAMModule()
+slam.start()
 
-    for det in detections:
-        print(det)
+for i in range(10):
+    slam.update()  # frame will be passed here later
+    print(slam.get_pose())
+    time.sleep(0.5)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+slam.stop()
 
-tracker.stop()
-cv2.destroyAllWindows()
